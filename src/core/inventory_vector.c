7f9272f8d9f02cbfe9d547dfc46091d79e6bb947
i0bz <andreiancheta101@gmail.com>
2026-04-16 00:34:34 +0800

    refactor: meaningful naming for the collection

--- src/core/inventory_vector.c
+++ src/core/inventory_vector.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static Vector* container = NULL;
+static Vector* inventory = NULL;
 
 Product* create_product(unsigned int id, char* name, char* category, float price, size_t quantity) {
     Product* product = malloc(sizeof(Product));
@@ -19,25 +19,25 @@ Product* create_product(unsigned int id, char* name, char* category, float price
 
 
 void open_inventory() {
-    if (container == NULL) 
-        container = create_vector(sizeof(Product));
+    if (inventory == NULL) 
+        inventory = create_vector(sizeof(Product));
 }
 
 void close_inventory() {
-    if (container == NULL) 
-        free(container);
+    if (inventory == NULL) 
+        free(inventory);
 }
 
 
 
 
 void add_to_inventory(Product* product) {
-    push_to_vector(container, product);
+    push_to_vector(inventory, product);
 }
 
 Product* retrieve_from_inventory(ID id) {
-    for (size_t i = 0; i < container->usage + 1; i++) {
-        Product* product = (Product*) at_vector(container, i);
+    for (size_t i = 0; i < inventory->usage + 1; i++) {
+        Product* product = (Product*) at_vector(inventory, i);
         if (product->id == id) return product;
     }
 
@@ -45,19 +45,19 @@ Product* retrieve_from_inventory(ID id) {
 }
 
 void remove_from_inventory(ID id) {
-    for (size_t i = 0; i < container->usage + 1; i++) {
-        Product* product = (Product*) at_vector(container, i);
-        if (product->id == id) remove_from_vector(container, i);
+    for (size_t i = 0; i < inventory->usage + 1; i++) {
+        Product* product = (Product*) at_vector(inventory, i);
+        if (product->id == id) remove_from_vector(inventory, i);
     }
 }
 
 size_t inventory_usage() {
-    return container->usage;
+    return inventory->usage;
 }
 
 void for_every_item(void(*func)(void*)) {
-    for (size_t i = 0; i < container->usage; i++) {
-        Product* product = (Product*) at_vector(container, i);
+    for (size_t i = 0; i < inventory->usage; i++) {
+        Product* product = (Product*) at_vector(inventory, i);
         func(product);
     }
 }
