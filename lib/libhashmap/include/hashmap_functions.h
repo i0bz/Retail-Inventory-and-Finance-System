ce84371a2639a5b6f078bdebe55bb526875eaa27
i0bz <andreiancheta101@gmail.com>
2026-04-17 19:26:05 +0800

    extracted my generic containers and made their own repos

--- lib/libhashmap/include/hashmap_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef HASHMAP_FUNCTIONS_H
-#define HASHMAP_FUNCTIONS_H
-
-#include "m_hashmap.h"
-
-bool compare_keys (void* key_1, void* key_2);
-size_t hash_function(Hashmap* map, void* key);
-
-#endif
